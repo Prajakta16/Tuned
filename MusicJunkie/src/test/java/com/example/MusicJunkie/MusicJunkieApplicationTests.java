9264ac2a85081520dacca3fbc99d099f98c8c48e
Prajakta Dharme <dharme.p@husky.neu.edu>
2020-04-10 14:11:07 -0700

    Adding project from spring initializer

---
+++ MusicJunkie/src/test/java/com/example/MusicJunkie/MusicJunkieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MusicJunkie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicJunkieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
