e9e3ff228c004aa533346cd1745f8beac900a5c1
Prajakta Dharme <dharme.p@husky.neu.edu>
2020-04-16 13:34:23 -0700

    Changes in Playlist controoler

--- Tuned/src/main/java/com/example/Tuned/controller/PlaylistController.java
+++ Tuned/src/main/java/com/example/Tuned/controller/PlaylistController.java
@@ -1,13 +1,12 @@
 package com.example.Tuned.controller;
 
 import com.example.Tuned.model.Playlist;
+import com.example.Tuned.model.Song;
 import com.example.Tuned.repository.PlaylistRepository;
+import com.example.Tuned.repository.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +16,20 @@ public class PlaylistController {
     @Autowired
     PlaylistRepository playlistRepository;
 
+    @Autowired
+    SongRepository songRepository;
+
     @PostMapping("/api/playlist")
     public Playlist createPlaylist(@RequestBody Playlist playlist){
+        return playlistRepository.save(playlist);
+    }
+
+    @PostMapping("/api/playlist/{playlist_id}/song/{song_id}")
+    public Playlist createPlaylist(@PathVariable("playlist_id") int playlist_id, @PathVariable("song_id") int song_id){
+        Playlist playlist = playlistRepository.findById(playlist_id).get();
+        Song song = songRepository.findById(song_id).get();
+        playlist.addSong(song);
+        songRepository.save(song);
         return playlistRepository.save(playlist);
     }
 
@@ -33,5 +44,11 @@ public class PlaylistController {
     @GetMapping("/api/playlists/all")
     public List<Playlist> findAllPlaylists() {
         return (List<Playlist>) playlistRepository.findAll();
+    }
+
+    @DeleteMapping("api/playlist/{playlist_id}")
+    public void deletePlaylistById(@PathVariable("playlist_id") int playlist_id) {
+        Playlist playlist = playlistRepository.findById(playlist_id).get();
+        playlistRepository.delete(playlist);
     }
 }
--- Tuned/src/main/java/com/example/Tuned/controller/SongController.java
+++ Tuned/src/main/java/com/example/Tuned/controller/SongController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @Service
 public class SongController {
 
