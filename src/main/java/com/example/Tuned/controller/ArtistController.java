400c930bdc9dc14f201a64b6127eecdc6be3d837
Prajakta Dharme <prajakta@Prajaktas-MacBook-Pro.local>
2020-04-19 16:57:28 -0700

    Adding cors in artist controller

--- src/main/java/com/example/Tuned/controller/ArtistController.java
+++ src/main/java/com/example/Tuned/controller/ArtistController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Service
 @RestController
 public class ArtistController {
